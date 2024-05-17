3afc38fd55f89a780b2745267d0287b38c24abbe
changsoon123 <sool0458@naver.com>
2024-05-17 23:11:57 +0900

    chat function debug

--- src/main/java/com/soon/chat/controller/MessageController.java
+++ src/main/java/com/soon/chat/controller/MessageController.java
@@ -2,11 +2,15 @@ package com.soon.chat.controller;
 
 import com.soon.chat.entity.Message;
 import com.soon.chat.service.MessageService;
+import com.soon.jwt.TokenProvider;
+import com.soon.jwt.TokenUserInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/message")
@@ -15,10 +19,23 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @Autowired
+    private TokenProvider tokenProvider;
+
     @CrossOrigin
     @GetMapping("/{room}")
     public ResponseEntity<List<Message>> getMessages(@PathVariable String room) {
         return ResponseEntity.ok(messageService.getMessages(room));
+    }
+
+    @GetMapping("/message/userinfo")
+    public ResponseEntity<?> checkPermission( @RequestHeader("Authorization") String token) {
+
+
+        TokenUserInfo userInfo = tokenProvider.validateAndReturnTokenUserInfo(token.substring(7));
+
+
+        return ResponseEntity.ok(userInfo.getUserNick());
     }
 
 }
